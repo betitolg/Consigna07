8a4d960a89549236b8e467d92c8888c2a9fed0f6
Daniel Lopez <betitolg@hotmail.com>
2022-07-29 15:24:40 -0500

    Last commit for Consigna07

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -7,33 +7,56 @@ public class CameraManager : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject[] cameras;
+    public  bool status = true;
+
     void Start()
     {
-        
+          cameras[0].SetActive(status);
+           cameras[1].SetActive(!status);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         if (Input.GetKeyDown(KeyCode.F1))
         {
-            cameras[0].SetActive(true);
-            cameras[1].SetActive(false);
-            cameras[2].SetActive(false);
+            
+ status=!status;
+if (status)
+{
+    cameras[0].SetActive(status);
+           cameras[1].SetActive(!status);
+}else{
+    cameras[0].SetActive(status);
+           cameras[1].SetActive(!status);
+}
+
         }
-        
-        if (Input.GetKeyDown(KeyCode.F2))
-        {
-            cameras[0].SetActive(false);
-            cameras[1].SetActive(true);
-            cameras[2].SetActive(false);
-        }
-        if (Input.GetKeyDown(KeyCode.F3))
-        {
-            cameras[0].SetActive(false);
-            cameras[1].SetActive(false);
-            cameras[2].SetActive(true);
-        }
-        
+
+ 
+
     }
+
+
+
+        
+       
+           
+      
+        // if (Input.GetKeyDown(KeyCode.F2))
+        // {
+        //     cameras[0].SetActive(false);
+        //     cameras[1].SetActive(true);
+        //     cameras[2].SetActive(false);
+        // }
+        // if (Input.GetKeyDown(KeyCode.F3))
+        // {
+        //     cameras[0].SetActive(false);
+        //     cameras[1].SetActive(false);
+        //     cameras[2].SetActive(true);
+        // }
+        
+    
 }
